263c38b94fadb462e56e3c3dd439c70e223b99d6
Steff Beckers <steff@steffbeckers.eu>
2021-10-05 22:25:20 +0200

    Greeter service client added in Blazor app

---
+++ gRPC.Client.Blazor/Models/GreeterInput.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gRPC.Client.Blazor.Models;
+
+public class GreeterInput
+{
+    [Required]
+    [StringLength(50, ErrorMessage = "Name is too long.")]
+    public string Name { get; set; }
+}
--- gRPC.Server/Services/GreeterService.cs
+++ gRPC.Server/Services/GreeterService.cs
@@ -6,10 +6,19 @@ namespace gRPC.Server.Services;
 
 public class GreeterService : GreeterBase
 {
+    private readonly ILogger<GreeterService> _logger;
+
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
     public override Task<HelloReply> SayHello(
         HelloRequest request,
         ServerCallContext context)
     {
+        _logger.LogInformation($"Greeting {request.Name}");
+
         return Task.FromResult(new HelloReply
         {
             Message = "Hello " + request.Name
