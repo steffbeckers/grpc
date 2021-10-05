5a009c7450ab70dc26a5e887f1038c866dea2530
Steff Beckers <steff@steffbeckers.eu>
2021-10-05 21:57:28 +0200

    Greeter service added to client

---
+++ gRPC.Client.Blazor/Data/GreeterService.cs
@@ -0,0 +1,20 @@
+﻿using gRPC.Greeter;
+using Grpc.Net.Client;
+using static gRPC.Greeter.Greeter;
+
+namespace gRPC.Client.Blazor.Data;
+
+public class GreeterService
+{
+    private readonly GreeterClient _client;
+
+    public GreeterService(IConfiguration configuration)
+    {
+        _client = new GreeterClient(GrpcChannel.ForAddress(configuration.GetValue<string>("gRPC:Endpoint")));
+    }
+
+    public async Task<HelloReply> SayHelloAsync(string name)
+    {
+        return await _client.SayHelloAsync(new HelloRequest { Name = name });
+    }
+}
--- gRPC.Client.Blazor/Program.cs
+++ gRPC.Client.Blazor/Program.cs
@@ -4,18 +4,17 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<GreeterService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
@@ -25,8 +24,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-
 app.MapBlazorHub();
+
 app.MapFallbackToPage("/_Host");
 
 app.Run();
